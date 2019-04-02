fd234623b911bb0e24059d435eac7e23e2eb0607
Kristof Buts <github@kristofbuts.be>
2019-04-02 06:57:25 +0000

    Front-end 2.0

--- UI-MVC/webpack.config.js
+++ UI-MVC/webpack.config.js
@@ -1,6 +1,9 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
 	entry: {
-		site: './wwwroot/js/site.js'
+		site: './wwwroot/js/site.js',
+		validation: './wwwroot/js/validation.js'
 	},
 	output: {
 		filename: '[name].entry.js',
@@ -10,12 +13,18 @@ module.exports = {
 	mode: 'development',
 	module: {
 		rules: [
-			{ test: /\.scss$/, loader: "style-loader!css-loader!sass-loader" },
-			{ test: /\.css$/, loader: "style-loader!css-loader" },
+			// { test: /\.scss$/, loader: "style-loader!css-loader!sass-loader" },
+			// { test: /\.css$/, loader: "style-loader!css-loader" },
+			{ test: /\.(sa|sc|c)ss$/, use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader", "sass-loader"] },
 			{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
 			{ test: /\.(woff|woff2)$/, loader:"url-loader?prefix=font/&limit=5000" },
 			{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
 			{ test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" }
 		]
-	}
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "[name].css"
+		})
+	]
 };
