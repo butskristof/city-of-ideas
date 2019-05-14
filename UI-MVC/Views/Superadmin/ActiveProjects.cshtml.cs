4564b6926454cd1bd8019099cbb8a4649188b7e0
Jordy Bruyns <jordy.bruyns@student.kdg.be>
2019-05-14 20:32:19 +0000

    superadmin

---
+++ UI-MVC/Views/Superadmin/ActiveProjects.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COI.UI.MVC.Views.Superadmin
+{
+    public class ActiveProjects : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
