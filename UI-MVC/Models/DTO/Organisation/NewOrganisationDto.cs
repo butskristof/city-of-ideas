88ec592598e1f7eb11ca0df133f1a62f9bd776f7
Kristof Buts <github@kristofbuts.be>
2019-05-20 16:05:29 +0000

    Organisation styling

--- UI-MVC/Models/DTO/Organisation/NewOrganisationDto.cs
+++ UI-MVC/Models/DTO/Organisation/NewOrganisationDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace COI.UI.MVC.Models.DTO.Organisation
 {
 	public class NewOrganisationDto
 	{
 		public string Name { get; set; }
 		public string Identifier { get; set; }
+		[RegularExpression("^#[0-9a-fA-F]{6}$")]
+		public string Color { get; set; }
 	}
 }
--- UI-MVC/Models/DTO/Organisation/OrganisationDto.cs
+++ UI-MVC/Models/DTO/Organisation/OrganisationDto.cs
@@ -9,6 +9,8 @@ namespace COI.UI.MVC.Models.DTO.Organisation
 		public string Name { get; set; }
 		public string Identifier { get; set; }
 		public string LogoLocation { get; set; }
+		public string ImageLocation { get; set; }
+		public string Color { get; set; }
 		public List<ProjectMinDto> Projects { get; set; }
 	}
 }
